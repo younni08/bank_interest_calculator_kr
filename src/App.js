ebb3c0dadb8de54e3ee934ab52a6c08df259cb15
younni08 <younni08@gmail.com>
2020-05-16 13:55:52 +0900

    ad added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './default.css';
 import IntCalc from "./componnents/interest_calculator"
 
+//<div className="left_ad">
+//rhk
+//</div>
+
 function App() {
   return (
     <div className="app_wapper">
       <div>
-        <div className="left_ad">
-          광고
-        </div>
         <div className="content">
           <IntCalc />
         </div>
